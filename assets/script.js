a16fa3c472cc7ad94f55b805bbe930e061dc994a
asw <hase127x@gmail.com>
2024-05-03 21:07:52 -0400

    level up & down buttons added

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,31 @@
 let lvl = 1
 
+
+const lvlUP = document.querySelector('#Up');
+const lvlDown = document.querySelector('#Down');
+const lvlf = document.querySelector('#lvl');
+
+//updates level ALLEGEDLY
+function setlvlText() {
+    lvlf.textContent = lvl;
+}
+lvlUP.addEventListener('click', function () {
+    //label later
+    lvl++;
+    setlvlText();
+});
+
+
+lvlDown.addEventListener('click', function () {
+    if (lvl > 1) {
+        lvl --;
+        setlvlText();
+    }
+    console.log("efsdf");
+});
+
+
+
 let str = 1; 
 let dex = 1;
 let con = 1;
@@ -9,7 +35,15 @@ let cha = 1;
 
 
 
+// function editLVL ('click', 
+
+
+
+
 // add stat variables 
+
+
+
 
 // compile into array?
 
