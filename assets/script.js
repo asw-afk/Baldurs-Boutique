a842d671697cdbe8afbcb2614baa02375b0ffd88
asw <annasilverwolff@gmail.com>
2024-06-14 00:05:02 -0400

    changing base stats to class

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,19 @@ const lvlUP = document.querySelector('#Up');
 const lvlDown = document.querySelector('#Down');
 const lvlf = document.querySelector('#lvl');
 
-//updates level ALLEGEDLY
+//updates level
 function setlvlText() {
     lvlf.textContent = lvl;
 }
 lvlUP.addEventListener('click', function () {
-    //label later
+    //Increase Level
     lvl++;
     setlvlText();
 });
 
 
 lvlDown.addEventListener('click', function () {
+    //Decrease Level
     if (lvl > 1) {
         lvl --;
         setlvlText();
@@ -24,14 +25,33 @@ lvlDown.addEventListener('click', function () {
     console.log("efsdf");
 });
 
+//Add all level one information
+
+//Stat Variable
+// let str = 10; 
+// let dex = 10;
+// let con = 10;
+// let int = 10;
+// let wis = 10;
+// let cha = 10;
+
+class Stats {
+    constructor(str, dex, con, int, wis, cha) {
+        this.str = str;
+        this.dex = dex;
+        this.con = con;
+        this.int = int;
+        this.wis = wis;
+        this.cha = cha;
+    }
+
+}
+
+//Discuss point build later
 
 
-let str = 1; 
-let dex = 1;
-let con = 1;
-let int = 1;
-let wis = 1;
-let cha = 1;
+
+
 
 
 
@@ -45,7 +65,7 @@ let cha = 1;
 
 
 
-// compile into array?
-
-// make array affected by user input
-
+// implement fetch to retrieve data from the bg3 wiki
+//get it to display the data in the search menu
+//fetch the data then display relevant data in an array(?)
+//does it have to be an array? i think so 
