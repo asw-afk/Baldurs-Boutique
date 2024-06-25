6b1bfbf75e9255f1effd9b66e44ee5302b607815
asw <annasilverwolff@gmail.com>
2024-06-25 04:27:19 -0400

    added class structure for classes, added a cantrip.js

---
+++ assets/class script/class.js
@@ -0,0 +1,20 @@
+class Class {
+    constructor() {
+        this.attribute = attribute,
+        this.actions, 
+        this.armour,
+        this.cantrips,
+        this.classFeats,
+        this.hp,
+        this.initiativeMod,
+        this.proficiencies,
+        this.skills,
+        this.spells
+    
+    }
+    setAttribute(attribute){
+        this.attribute = attribute;
+    }
+    
+
+}
