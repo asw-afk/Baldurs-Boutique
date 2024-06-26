0030c00db668cc08e00ce5f3224162432c23956b
asw <annasilverwolff@gmail.com>
2024-06-26 03:15:56 -0400

    Added sublist selection html

--- assets/class script/class.js
+++ assets/class script/class.js
@@ -1,20 +1,35 @@
-class Class {
-    constructor() {
-        this.attribute = attribute,
-        this.actions, 
-        this.armour,
-        this.cantrips,
-        this.classFeats,
-        this.hp,
-        this.initiativeMod,
-        this.proficiencies,
-        this.skills,
-        this.spells
-    
-    }
-    setAttribute(attribute){
-        this.attribute = attribute;
-    }
-    
+class Classes {
+  constructor(
+    attribute,
+    actions,
+    armour,
+    cantrips,
+    classFeats,
+    hp,
+    initiativeMod,
+    proficiencies,
+    skills,
+    spells
+  ) {
+    (this.attribute = []),
+      this.actions,
+      this.armour,
+      this.cantrips,
+      this.classFeats,
+      this.hp,
+      this.initiativeMod,
+      this.proficiencies,
+      this.skills,
+      this.spells;
+  }
+}
 
+class Cleric extends Classes {
+  render() {
+    (attribute = [15, 10, 13, 8, 17, 12]),
+      (actions = ""),
+      (cantrips = ""),
+      (classFeats = "lvl1 Spell Slots");
+    ("Domain Spells");
+  }
 }
--- assets/script.js
+++ assets/script.js
@@ -53,3 +53,4 @@ function openSheet(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
