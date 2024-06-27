fc6e39681e4d6129bacc7bcac7fed9f9d43eae90
asw <annasilverwolff@gmail.com>
2024-06-27 01:35:14 -0400

    Added two class sheets

--- assets/class script/class.js
+++ assets/class script/class.js
@@ -13,23 +13,47 @@ class Classes {
   ) {
     (this.attribute = []),
       this.actions,
-      this.armour,
       this.cantrips,
-      this.classFeats,
+      this.classFeatures,
       this.hp,
       this.initiativeMod,
-      this.proficiencies,
-      this.skills,
+      this.weaponsProf,
+      this.armourProf,
+      this.skillsProf
       this.spells;
   }
 }
 
-class Cleric extends Classes {
-  render() {
-    (attribute = [15, 10, 13, 8, 17, 12]),
-      (actions = ""),
-      (cantrips = ""),
-      (classFeats = "lvl1 Spell Slots");
-    ("Domain Spells");
+//Unarmored Defence adds you con modifier to your armor class while not wearing armor
+//heavy armor impedes your rage
+class Barbarian extends Classes { 
+  constructor()
+  {
+    (attribute = [17, 13, 15, 8, 12, 10]),
+    (actions = 'Rage'),
+    cantrips = 'n/a',
+    classFeatures = 'Unarmored Defence',
+    hp = 14,
+    initiativeMod = +1,
+    weaponsProf = ['Simple Weapons', 'Martial Weapons', 'Longswords', 'Shortswords', 'Longbows', 'Shortbows'],
+    armourProf = ['Light Armor', 'Medmium Armour', 'Shields'],
+    skillsProf = ['Athletics', 'Sleight of Hand', 'Perception', 'Deception', 'Intimidation']
   }
 }
+
+class Cleric extends Classes {
+  constructor() {
+    (attribute = [15, 10, 13, 8, 17, 12]),
+    actions ='n/a',
+    cantrips = ['Resistence', 'Guidance', 'Sacred Flame'],
+    classFeatures = ['LvlOne SpellSlots', 'Domain Spells'],
+    hp = 9,
+    initiativeMod = 'n/a',
+    weaponsProf = ['Simple Weapons', 'Morningstars', 'Longswrods', 'Shortswords', 'Longbows', 'Shortbows' ],
+    armourProf = ['Light Armor', 'Medium Armor', 'Shields', 'Heavy Armor'],
+    skillsProf = ['Sleight of Hand', 'Religion', 'Insight', 'Perception', 'Deception'],
+    spells = ['Guiding Bolt', 'Healing Word', 'Inflict Wound', 'Shield of Faith', 'Bane']
+  }
+}
+
+
