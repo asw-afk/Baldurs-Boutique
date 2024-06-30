d857e7132dd70b78eb5baa4cbc635fd4e0f5a630
asw <annasilverwolff@gmail.com>
2024-06-30 04:47:26 -0400

    Server file set up

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require("express")
+const path = require('path');
+
+const PORT = 3000; 
+
+const app = express();
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
 
