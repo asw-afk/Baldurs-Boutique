cb81894b9b8a7bd1fa3a1b57d4ac24a0d76186ff
asw <annasilverwolff@gmail.com>
2024-09-17 21:12:58 -0400

    fixed bug, found new bug

---
+++ client/dev/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+export default function Header(){
+    return (
+
+        <Header className="header">
+    
+        <p>Text</p>
+       
+        </Header>
+
+
+    )
+}
--- client/dev/src/main.jsx
+++ client/dev/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// import Header from './components/Header/Header.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
+
     <App />
-  </StrictMode>,
+ 
 )
