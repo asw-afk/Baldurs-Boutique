3752246b4bc78256be4722ca347e50bbed4302a2
asw <annasilverwolff@gmail.com>
2024-09-17 20:33:09 -0400

    Bootstrap errors

--- client/dev/src/App.jsx
+++ client/dev/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+      
+
       <div>
        
       </div>
