8bfb5208d571ae0e2e83f0f18751980fe9a541e5
masoudwebdeveloper <masoudwebdeveloper@gmail.com>
2024-09-19 21:01:47 -0400

    schemas foldet- resolvers and typedefs added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,85 @@
+const { User, Character } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate('thoughts');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('thoughts');
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { thoughtId }) => {
+      return Thought.findOne({ _id: thoughtId });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('thoughts');
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addCharacter: async (parent, { characterName }, context) => {
+      if (context.user) {
+        const character = await Character.create({
+          characterName,
+          characterAuthor: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { characters: character._id } }
+        );
+
+        return character;
+      }
+      throw AuthenticationError;
+    },
+    removeCharacter: async (parent, { characterId }, context) => {
+      if (context.user) {
+        const character = await character.findOneAndDelete({
+          _id: characterId,
+          characterAuthor: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { character: character._id } }
+        );
+
+        return character;
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
